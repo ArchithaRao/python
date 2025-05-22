5e9b34a0a1f711519625d410a5629fc747aae117
Architha <168562220+ArchithaRao@users.noreply.github.com>
2025-05-22 19:23:43 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,80 @@
+#g=120
+#print(type(g))
+#g=120; h=220
+#print(g,h)
+#print(id(g),id(h))
+#a=10
+#b=20
+#c=25
+#b=30
+#print(b)
+#Ternary operator
+#a=10
+#b=20
+#res=a if a>b else b
+#print(res)
+
+#membership
+#h=[1,2,3,4,5,10]
+#if 10 in h:
+ #   print(True)
+#else:
+ #   print(False)
+#bitwise
+#a=0b10101
+#b=0b01010
+#print(a&b)
+#a=0b10101
+#b=0b01010
+#print(a|b)
+#a=0b10101
+#b=0b01010
+#print(a^b)
+#a=0b101010
+#b=2
+#print(a<<b)
+#print(a>>b)
+#l=2 / 3j
+#print(l)
+#print(type(l))
+#a = 2 + 3j
+#b = 1 - 4j
+
+#print(a)     
+#print(type(a))
+#l=2 * 3j
+#print(l)
+#print(type(l))
+
+    
+#a=input("enter")
+#print("a")
+#print(type(a))
+
+#a=input("enter:")
+#print(a)
+#print(type(a))
+
+#l=2.3e2
+#print(l)
+#print(type(l))
+
+#a=10
+#b=20
+#res=a<b
+#res1=a>b
+#print(res)
+#print(res1)
+#print(type(res))
+#print(type(res1))
+#print(id(res))
+
+s="python developer"
+s1=s.count('y')
+print(s1)
+s2=s.replace('python','java')
+print(s2)
+print(s.upper())
+print(s.lower())
+print(s.startswith('python'))
+print(s.endswith('developer'))
